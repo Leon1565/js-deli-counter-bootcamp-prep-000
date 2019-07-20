96a50912cdde95df3b8b7bd3de710b86a49228e4
Cha Yang <chachaiyang3@gmail.com>
2019-07-20 06:47:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var currentLine = [1, 2, 3,];
+
+function takeANumber(nextPerson, currentLine){
+  return `Welcome, ${nextPerson}. 
+  You are number ${currentLine} in line.`
+}
+takeANumber('Ada', currentLine[0]);
